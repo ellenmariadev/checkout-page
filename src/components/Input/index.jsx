83241ab0f5d93f5d90a6f5e5b26ed1f4eb8987ef
ellenmariadev <ellenmariadev@gmail.com>
2023-07-20 00:19:14 -0300

    refactor: Input component add new props

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,17 +1,27 @@
+import { forwardRef } from "react";
 import S from "./styles.module.scss";
 
-const Input = (props) => {
+const Input = forwardRef(({ errors, ...props }, ref) => {
+  let { name } = props;
+  const renderErrorMessage = () => {
+    return errors[name]?.message || "";
+  };
+
   return (
     <div className={S.relative}>
       <input
         {...props}
         type={props.type}
+        name={props.name}
         className={`${S.input} ${props.newclass}`}
         placeholder={props.placeholder}
+        ref={ref}
+        autoComplete="off"
       />
       <label className={S.label}>{props.label}</label>
+      <p className="error__input">{renderErrorMessage()}</p>
     </div>
   );
-};
+});
 
 export default Input;
