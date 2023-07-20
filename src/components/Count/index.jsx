729144c66fee6d7fa553789e5070087bfb9fa3bc
ellenmariadev <ellenmariadev@gmail.com>
2023-07-20 00:21:44 -0300

    feat[component]: Count

---
+++ src/components/Count/index.jsx
@@ -0,0 +1,87 @@
+import { useState, useRef, forwardRef } from "react";
+import S from "./styles.module.scss";
+
+export const Count = forwardRef(
+  ({ title, setCounts, setValue, errors }, ref) => {
+    const [count, setCount] = useState(0);
+    const inputRef = useRef(null);
+
+    const handleIncrement = (e) => {
+      e.preventDefault();
+      setCount((prevCount) => prevCount + 1);
+      setCounts((prevCounts) =>
+        prevCounts.map((c) =>
+          c.title === title ? { ...c, count: c.count + 1 } : c,
+        ),
+      );
+      if (title === "Adultos") {
+        setValue("Adultos", count + 1);
+      } else if (title === "Crianças") {
+        setValue("Crianças", count + 1);
+      }
+    };
+
+    const handleDecrement = (e) => {
+      e.preventDefault();
+      if (count > 0) {
+        setCount((prevCount) => prevCount - 1);
+        setCounts((prevCounts) =>
+          prevCounts.map((c) =>
+            c.title === title ? { ...c, count: c.count - 1 } : c,
+          ),
+        );
+        if (title === "Adultos") {
+          setValue("Adultos", count - 1);
+        } else if (title === "Crianças") {
+          setValue("Crianças", count - 1);
+        }
+      }
+    };
+
+    const handleInputChange = (e) => {
+      const value = parseInt(e.target.value);
+      setCount(value);
+      setCounts((prevCounts) =>
+        prevCounts.map((c) => (c.title === title ? { ...c, count: value } : c)),
+      );
+      if (title === "Adultos") {
+        setValue("Adultos", value);
+      } else if (title === "Crianças") {
+        setValue("Crianças", value);
+      }
+    };
+
+    return (
+      <div className={S.count}>
+        <button
+          disabled={count > 0 ? false : true}
+          className={`${S.count__button} ${count > 0 ? "" : "disabled"}`}
+          onClick={handleDecrement}
+        >
+          -
+        </button>
+        <div className={`${S.count__card} ${errors[title] ? "error" : ""}`}>
+          <p>{title}</p>
+          <input
+            min={0}
+            max={50}
+            type="number"
+            value={isNaN(count) ? "" : count}
+            className={S.count__number}
+            onChange={handleInputChange}
+            inputMode="numeric"
+            ref={inputRef}
+            readOnly
+          />
+        </div>
+        <button
+          className={`${S.count__button}`}
+          title="Adicionar mais um"
+          onClick={handleIncrement}
+        >
+          +
+        </button>
+      </div>
+    );
+  },
+);
